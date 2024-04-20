823d2b5503bc4749821a86281d5cedf90135bdee
jessworks <48979913+jessworks@users.noreply.github.com>
2024-04-20 03:38:42 +0200

    fix: remove #

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ Det spelar ingen roll om jag lägger koden såhär
 
 gsap.to('.cls-1', { fill: '#e305ab', duration: 1, repeat: -1, yoyo: true});
 
-Eller såhär
+Eller såhär*/
 
 const o = document.querySelector('#o');
 const k = document.querySelector('#k');
 
-gsap.to('#o', { fill: '#e305ab', duration: 1, repeat: -1, yoyo: true});
-gsap.to('#k', { fill: '#e305ab', duration: 1, repeat: -1, yoyo: true});
+gsap.to('o', { fill: '#e305ab', duration: 1, repeat: -1, yoyo: true});
+gsap.to('k', { fill: '#e305ab', duration: 1, repeat: -1, yoyo: true});
 
-Den funkar i HTML-filen på båda sätten, men inte här. När den liggeri HTML-filen så funkar den i live share och i codepen, men inte på github pages. Det är något grundläggande jag inte förstår.
+/*Den funkar i HTML-filen på båda sätten, men inte här. När den liggeri HTML-filen så funkar den i live share och i codepen, men inte på github pages. Det är något grundläggande jag inte förstår.
 */
