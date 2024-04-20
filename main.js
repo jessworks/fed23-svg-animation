cc4f955559c324875745ec0895a3a92a059da596
jessworks <48979913+jessworks@users.noreply.github.com>
2024-04-20 03:34:01 +0200

    feat: main.js added

---
+++ main.js
@@ -0,0 +1,15 @@
+/*
+Det spelar ingen roll om jag lägger koden såhär
+
+gsap.to('.cls-1', { fill: '#e305ab', duration: 1, repeat: -1, yoyo: true});
+
+Eller såhär
+
+const o = document.querySelector('#o');
+const k = document.querySelector('#k');
+
+gsap.to('#o', { fill: '#e305ab', duration: 1, repeat: -1, yoyo: true});
+gsap.to('#k', { fill: '#e305ab', duration: 1, repeat: -1, yoyo: true});
+
+Den funkar i HTML-filen på båda sätten, men inte här. När den liggeri HTML-filen så funkar den i live share och i codepen, men inte på github pages. Det är något grundläggande jag inte förstår.
+*/
