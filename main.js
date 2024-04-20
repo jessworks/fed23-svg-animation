e3f46d2b8bf7eface67adf0fc08528631eef0488
jessworks <48979913+jessworks@users.noreply.github.com>
2024-04-20 03:51:34 +0200

    fix: moved script back to HTML

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+/*
 const o = document.querySelector('#o');
 const k = document.querySelector('#k');
 
 gsap.to('o', { fill: '#e305ab', duration: 1, repeat: -1, yoyo: true});
 gsap.to('k', { fill: '#e305ab', duration: 1, repeat: -1, yoyo: true});
+*/
